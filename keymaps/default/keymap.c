3b4db3dcd78ea4ae4f4aed004a65f63ff47eeee9
Jay Wu <yoj845@gmail.com>
2021-10-27 08:52:36 +0800

    💄 Updated the keymap

--- keymaps/default/keymap.c
+++ keymaps/default/keymap.c
@@ -90,14 +90,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_BSPC,KC_A   ,KC_R   ,KC_S   ,KC_T   ,KC_G   ,                KC_M   ,KC_N   ,KC_E   ,KC_I   ,KC_O   ,KC_QUOT,
     KC_LSFT,KC_Z   ,KC_X   ,KC_C   ,KC_D   ,KC_V   ,                KC_K   ,KC_H   ,SR_COMM,SR_DOT ,SR_QUES,KC_RSFT,
                             KC_LALT,TD_LWIN,KC_LCTL,KC_LSFT,SFT_ENT,ST_SPC ,TT(_FN),KC_RALT,
-                            SR_ESC ,                QMKBEST,_______,                 SR_IME
+                            SR_ESC ,                QMKBEST,G(KC_V),                 SR_IME
     ),
     [_FN] = LAYOUT(
     KC_F1  ,KC_F2  ,KC_F3  ,KC_F4  ,KC_F5  ,KC_F6  ,                KC_F7  ,KC_F8  ,KC_F9  ,KC_F10 ,KC_F11 ,KC_F12 ,
     _______,KC_PSLS,KC_7   ,KC_8   ,KC_9   ,KC_PMNS,                KC_PGUP,KC_HOME,KC_UP  ,KC_END ,KC_INS ,KC_VOLU,
     _______,KC_PAST,KC_4   ,KC_5   ,KC_6   ,KC_PPLS,                KC_PGDN,KC_LEFT,KC_DOWN,KC_RGHT,KC_DEL ,KC_VOLD,
-    _______,_______,KC_1   ,KC_2   ,KC_3   ,_______,                KC_WHOM,KC_WBAK,KC_WREF,KC_WFWD,KC_WSCH,KC_MUTE,
-                            KC_EQL ,TD_DOT ,KC_0   ,KC_PENT,_______,_______,TT(_FN),_______,
+    _______,C(KC_Z),KC_1   ,KC_2   ,KC_3   ,KC_0   ,                KC_WHOM,KC_WBAK,KC_WREF,KC_WFWD,KC_WSCH,KC_MUTE,
+                            KC_EQL ,TD_DOT ,KC_0   ,KC_PENT,KC_RSFT,KC_RCTL,TT(_FN),KC_RALT,
                             KC_CALC,                _______,_______,                KC_CAPS
     )
 };
