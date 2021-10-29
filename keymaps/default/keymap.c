985e1d929c1120f7f6331bcf987741d41341be1a
Jay Wu <yoj845@gmail.com>
2021-10-29 22:02:31 +0800

    💄 Updated the keymap

--- keymaps/default/keymap.c
+++ keymaps/default/keymap.c
@@ -24,7 +24,8 @@ enum layer_names {
 // Defines the keycodes used by our macros in process_record_user
 enum custom_keycodes {
     QMKBEST = SAFE_RANGE,
-    ALT_TAB
+    ALT_TAB,
+    CUT_PST
 };
 
 // Tap Dance declarations
@@ -50,8 +51,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_TAB ,KC_Q   ,KC_W   ,KC_F   ,KC_P   ,KC_B   ,                KC_J   ,KC_L   ,KC_U   ,KC_Y   ,KC_MINS,KC_EQL ,
     KC_BSPC,KC_A   ,KC_R   ,KC_S   ,KC_T   ,KC_G   ,                KC_M   ,KC_N   ,KC_E   ,KC_I   ,KC_O   ,KC_QUOT,
     KC_LSFT,KC_Z   ,KC_X   ,KC_C   ,KC_D   ,KC_V   ,                KC_K   ,KC_H   ,KC_COMM,KC_DOT ,KC_SLSH,KC_QUES,
-                            KC_LALT,TD_LWIN,KC_LCTL,KC_LSFT,KC_ENT ,ST_SPC ,MO(_FN),KC_INS ,
-                            TD_ESC ,                CPY_PST,XXXXXXX,                TD_IME
+                            KC_LALT,TD_LWIN,KC_LCTL,KC_LSFT,ST_SPC ,KC_ENT ,MO(_FN),KC_INS ,
+                            TD_ESC ,                CPY_PST,CUT_PST,                TD_IME
     ),
     [_FN] = LAYOUT(
     KC_F1  ,KC_F2  ,KC_F3  ,KC_F4  ,KC_F5  ,KC_F6  ,                KC_F7  ,KC_F8  ,KC_F9  ,KC_F10 ,KC_F11 ,KC_F12 ,
@@ -59,7 +60,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     _______,KC_PAST,KC_4   ,KC_5   ,KC_6   ,KC_PPLS,                KC_PGDN,KC_LEFT,KC_DOWN,KC_RGHT,KC_BRID,KC_VOLD,
     _______,KC_0   ,KC_1   ,KC_2   ,KC_3   ,XXXXXXX,                XXXXXXX,KC_WBAK,KC_WREF,KC_WFWD,KC_WSCH,KC_MUTE,
                             KC_EQL ,TD_DOT ,KC_0   ,KC_PENT,KC_RSFT,KC_RCTL,MO(_FN),KC_INS ,
-                            KC_CALC,                XXXXXXX,XXXXXXX,                KC_CAPS
+                            KC_CALC,                _______,_______,                KC_CAPS
     )
 };
 
@@ -120,8 +121,23 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
                 unregister_code(KC_TAB);
             }
             break;
+        case CUT_PST:
+            if (record->event.pressed) {
+                SEND_STRING(SS_LCTL("x"));
+            } else {
+                SEND_STRING(SS_LCTL("v"));
+            }
+            break;
     }
     return true;
+}
+
+void post_process_record_user(uint16_t keycode, keyrecord_t *record) {
+    switch (keycode) {
+    case KC_ESC:
+        clear_mods();
+        break;
+    }
 }
 
 // Key Overrides
