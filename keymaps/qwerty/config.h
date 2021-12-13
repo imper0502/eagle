df526121454773af7fe14a0db7fe667b0cbc48dc
Jay_NTUT_LAB1321 <t109598063@ntut.org.tw>
2021-12-13 17:33:49 +0800

    🔥 Removed empty files

--- keymaps/qwerty/config.h
+++
@@ -1,4 +0,0 @@
-/* If defined, GRAVE_ESC will always act as ESC when CTRL is held.
- * This is useful for the Windows task manager shortcut (ctrl+shift+esc).
- */
-#define GRAVE_ESC_CTRL_OVERRIDE
--- keymaps/standard/config.h
+++ keymaps/standard/config.h
@@ -1,7 +1,2 @@
 #define ENCODERS_PAD_A { D3 }
 #define ENCODERS_PAD_B { D2 }
-
-/* If defined, GRAVE_ESC will always act as ESC when CTRL is held.
- * This is useful for the Windows task manager shortcut (ctrl+shift+esc).
- */
-#define GRAVE_ESC_CTRL_OVERRIDE
