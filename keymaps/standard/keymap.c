7742b830342ab25d9f207d9b16a09f602280012f
Jay_NTUT_LAB1321 <t109598063@ntut.org.tw>
2022-01-02 16:21:14 +0800

    🔥 Removed typing timer & Refactored code

--- keymaps/standard/keymap.c
+++ keymaps/standard/keymap.c
@@ -16,7 +16,7 @@
 #include QMK_KEYBOARD_H
 
 enum layer_names {
-    _BS, QWT,
+    _BS, _QW,
     _FN, MY_COMMAND
 };
 
@@ -72,11 +72,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                                 KC_LALT,KC_LCTL,KC_RSFT,KC_BSPC,KC_ENT ,SFT_SPC,FNT_TAB,KC_DEL ,
                             KC_ESC ,                    CPY_PST,KC_INS ,                    TD_IME
     ),
-    [QWT] = LAYOUT(
+    [_QW] = LAYOUT(
         _______,_______,_______,_______,_______,_______,                _______,_______,_______,_______,_______,_______,
-        _______,_______,_______,KC_E   ,KC_R   ,KC_T   ,                KC_Y   ,KC_U   ,KC_I   ,KC_O   ,KC_P   ,KC_QUES,
-        _______,_______,KC_S   ,KC_D   ,KC_F   ,_______,                KC_H   ,KC_J   ,KC_K   ,KC_L   ,KC_MINS,KC_EQL ,
-        _______,_______,_______,_______,KC_V   ,KC_B   ,                KC_N   ,KC_M   ,_______,_______,_______,_______,
+        _______,KC_Q   ,KC_W   ,KC_E   ,KC_R   ,KC_T   ,                KC_Y   ,KC_U   ,KC_I   ,KC_O   ,KC_P   ,KC_QUES,
+        _______,KC_A   ,KC_S   ,KC_D   ,KC_F   ,KC_G   ,                KC_H   ,KC_J   ,KC_K   ,KC_L   ,KC_MINS,KC_EQL ,
+        _______,KC_Z   ,KC_X   ,KC_C   ,KC_V   ,KC_B   ,                KC_N   ,KC_M   ,_______,_______,_______,_______,
                                 _______,_______,_______,_______,_______,_______,_______,_______,
                             _______,                    _______,_______,                    _______
     ),
@@ -84,13 +84,13 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_F1  ,KC_F2  ,KC_F3  ,KC_F4  ,KC_F5  ,KC_F6  ,                KC_F7  ,KC_F8  ,KC_F9  ,KC_F10 ,KC_F11 ,KC_F12 ,
       S(KC_TAB),KC_PSLS,KC_7   ,KC_8   ,KC_9   ,KC_PMNS,                KC_BRIU,KC_HOME,KC_UP  ,KC_END ,KC_PGUP,KC_INS ,
         KC_ESC ,KC_PAST,KC_4   ,KC_5   ,KC_6   ,KC_PPLS,                KC_BRID,KC_LEFT,KC_DOWN,KC_RGHT,KC_PGDN,KC_DEL ,
-        KC_CALC,KC_PERC,KC_1   ,KC_2   ,KC_3   ,KC_DLR ,                KC_MPRV,KC_MPLY,KC_MNXT,KC_MUTE,KC_VOLD,KC_VOLU,
+        KC_CALC,KC_PERC,KC_1   ,KC_2   ,KC_3   ,KC_TAB ,                KC_MPRV,KC_MPLY,KC_MNXT,KC_MUTE,KC_VOLD,KC_VOLU,
                                 TD_EQL ,KC_PDOT,KC_0   ,_______,_______,KC_RSFT,KC_RCTL,KC_RALT,
-                            KC_PENT,                    KC_TAB ,_______,                    KC_CAPS
+                            KC_PENT,                    _______,_______,                    KC_CAPS
     ),
     [MY_COMMAND] = LAYOUT(
         XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,                XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,
-        XXXXXXX,TG(QWT),CG_NORM,TG(_FN),KC_PSCR,XXXXXXX,                XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,
+        XXXXXXX,TG(_QW),CG_NORM,TG(_FN),KC_PSCR,XXXXXXX,                XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,
         XXXXXXX,XXXXXXX, RESET ,KC_SLCK,CG_TOGG,XXXXXXX,                CG_SWAP,KC_NLCK,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,
         XXXXXXX,XXXXXXX,XXXXXXX,KC_CAPS,XXXXXXX,XXXXXXX,                XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,
                                 XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,
@@ -106,22 +106,13 @@ void keyboard_pre_init_user(void) {
     writePin(RXLED, LED_OFF);
 }
 
-uint16_t typing_timer = 0; // range: 0 ~ 65535
 void matrix_scan_user(void) {
     writePin(TXLED, IS_LAYER_OFF(_FN));
     writePin(RXLED, IS_LAYER_OFF(_FN));
-
-    if (IS_LAYER_ON(_FN)) {
-        if (timer_elapsed(typing_timer) > 60000) {
-            layer_off(_FN);
-        }
-    }
-    
-    get_mods() == MOD_MASK_SHIFT ? layer_on(MY_COMMAND) : layer_off(MY_COMMAND);
 }
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
-    typing_timer = timer_read();
+    get_mods() == MOD_MASK_SHIFT ? layer_on(MY_COMMAND) : layer_off(MY_COMMAND);
     return true;
 }
 
@@ -196,7 +187,6 @@ void td_copy_paste_reset(qk_tap_dance_state_t *state, void *user_data) {
 /* Retry Encoders */
 uint8_t mod_state;
 bool encoder_update_user(uint8_t index, bool clockwise) {
-    typing_timer = timer_read();
     mod_state = get_mods();
     switch (index) {
     case 0:
