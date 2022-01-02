65e3dbe1dae26c50eaf0c166f71d4b7ce45a6c78
Jay_NTUT_LAB1321 <t109598063@ntut.org.tw>
2022-01-02 16:07:50 +0800

    ✨ VIA supported

--- keymaps/standard/config.h
+++ keymaps/standard/config.h
@@ -1,3 +1,8 @@
+/* 
+ * If defined, GRAVE_ESC will always act as ESC when CTRL is held.
+ * This is useful for the Windows task manager shortcut (ctrl+shift+esc).
+ */
+#define GRAVE_ESC_CTRL_OVERRIDE
 #define TAPPING_TERM_PER_KEY
 #define ENCODERS_PAD_A { D3 }
 #define ENCODERS_PAD_B { D2 }
