d265cef7df9d558cc76635c940550563d6711604
Jay_NTUT_LAB1321 <t109598063@ntut.org.tw>
2022-01-18 01:22:59 +0800

    ⚡️ Kinetic Mouse Key

--- keymaps/standard/config.h
+++ keymaps/standard/config.h
@@ -4,3 +4,5 @@
 #define ONESHOT_TIMEOUT 5000  /* Time (in ms) before the one shot key is released */
 #define ENCODERS_PAD_A { D3 }
 #define ENCODERS_PAD_B { D2 }
+#define MK_KINETIC_SPEED
+#define MOUSEKEY_INTERVAL 72
