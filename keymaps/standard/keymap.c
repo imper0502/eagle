87fb4745194785add7790b79f57649aaef335523
Jay_NTUT_LAB1321 <t109598063@ntut.org.tw>
2022-01-18 18:03:01 +0800

    💄 Added mouse key layer, _GM

--- keymaps/standard/keymap.c
+++ keymaps/standard/keymap.c
@@ -16,7 +16,7 @@
 #include QMK_KEYBOARD_H
 
 enum layer_names {
-    _BS, _QW, NUM, GAME,
+    _BS, _QW, _GM, _NP,
     _FN, MY_COMMAND
 };
 
@@ -55,10 +55,12 @@ const key_override_t **key_overrides = (const key_override_t *[]) {
 };
 
 #define ALT_TAB TD(ALT_TABLE)
-#define CPY_PST TD(COPY_PASTE_FNLOCK_SCREENSHOT)
 #define TD_LANG TD(IME_CAPSLOCK)
+#define CPY_PST TD(COPY_PASTE_FNLOCK_SCREENSHOT)
 #define GUI_ESC LGUI_T(KC_ESC)
 #define OS_RSFT OSM(MOD_RSFT)
+#define GM_ESC  LT(_GM, KC_ESC)
+#define GM_TAB  LT(_GM, KC_TAB)
 #define FN_SPC  LT(_FN, KC_SPC)
 #define FN_RCTL LM(_FN, MOD_RCTL)
 #define FN_LALT LM(_FN, MOD_LALT)
@@ -67,11 +69,11 @@ const key_override_t **key_overrides = (const key_override_t *[]) {
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_BS] = LAYOUT(
         ALT_TAB,KC_GRV ,KC_AT  ,KC_HASH,KC_AMPR,KC_SLSH,                KC_BSLS,KC_LPRN,KC_LBRC,KC_LCBR,KC_LABK,KC_BTN4,
-        KC_TAB ,KC_Q   ,KC_W   ,KC_F   ,KC_P   ,KC_B   ,                KC_J   ,KC_L   ,KC_U   ,KC_Y   ,KC_MINS,KC_EQL ,
+        GM_TAB ,KC_Q   ,KC_W   ,KC_F   ,KC_P   ,KC_B   ,                KC_J   ,KC_L   ,KC_U   ,KC_Y   ,KC_MINS,KC_EQL ,
         GUI_ESC,KC_A   ,KC_R   ,KC_S   ,KC_T   ,KC_G   ,                KC_M   ,KC_N   ,KC_E   ,KC_I   ,KC_O   ,KC_QUES,
         KC_LSFT,KC_Z   ,KC_X   ,KC_C   ,KC_D   ,KC_V   ,                KC_K   ,KC_H   ,KC_COMM,KC_DOT ,KC_QUOT,KC_RALT,
                                 KC_LALT,KC_LCTL,OS_RSFT,KC_BSPC,KC_ENT ,FN_SPC ,FN_RCTL,FN_LALT,
-                           MO(GAME),                    CPY_PST,KC_INS ,                    TD_LANG
+                            GM_ESC,                     CPY_PST,KC_INS ,                    TD_LANG
     ),
     [_QW] = LAYOUT(
         _______,_______,_______,_______,_______,_______,                _______,_______,_______,_______,_______,_______,
@@ -81,34 +83,34 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                                 _______,_______,_______,_______,_______,_______,_______,_______,
                             _______,                    _______,_______,                    _______
     ),
-    [NUM] = LAYOUT(
-        _______,_______,_______,_______,_______,_______,                _______,_______,_______,_______,_______,_______,
-        _______,KC_PSLS,KC_7   ,KC_8   ,KC_9   ,KC_PMNS,                KC_PSLS,KC_7   ,KC_8   ,KC_9   ,KC_PMNS,_______,
-        KC_CALC,KC_PAST,KC_4   ,KC_5   ,KC_6   ,KC_PPLS,                KC_PAST,KC_4   ,KC_5   ,KC_6   ,KC_PPLS,KC_CALC,
-        _______,KC_PERC,KC_1   ,KC_2   ,KC_3   ,KC_EQL ,                KC_PERC,KC_1   ,KC_2   ,KC_3   ,KC_EQL ,_______,
-                                KC_0   ,KC_DOT ,_______,_______,_______,_______,KC_0   ,KC_DOT ,
-                            _______,                    _______,_______,                    _______
+    [_GM] = LAYOUT(
+        KC_ESC ,KC_1   ,KC_2   ,KC_3   ,KC_4   ,KC_5   ,                KC_6   ,KC_7   ,KC_8   ,KC_9   ,KC_0   ,TG(_GM),
+        KC_TAB ,KC_Q   ,KC_W   ,KC_E   ,KC_R   ,KC_T   ,                XXXXXXX,KC_WH_L,KC_MS_U,KC_WH_R,KC_WH_U,XXXXXXX,
+        KC_CAPS,KC_A   ,KC_S   ,KC_D   ,KC_F   ,KC_G   ,                XXXXXXX,KC_MS_L,KC_MS_D,KC_MS_R,KC_WH_D,XXXXXXX,
+        KC_LSFT,KC_Z   ,KC_X   ,KC_C   ,KC_V   ,KC_B   ,                XXXXXXX,KC_BTN6,KC_BTN7,KC_BTN8,XXXXXXX,XXXXXXX,
+                                _______,_______,_______,_______,KC_BTN4,KC_BTN1,KC_BTN3,KC_BTN2,
+                            _______,                    _______,KC_BTN5,                    XXXXXXX
     ),
-    [GAME] = LAYOUT(
-        KC_ESC ,KC_1   ,KC_2   ,KC_3   ,KC_4   ,KC_5   ,                KC_6   ,KC_7   ,KC_8   ,KC_9   ,KC_0   ,TG(GAME),
-        KC_TAB ,KC_Q   ,KC_W   ,KC_E   ,KC_R   ,KC_T   ,                KC_ACL2,KC_BTN1,KC_MS_U,KC_BTN2,KC_BTN3,KC_BTN6,
-        KC_CAPS,KC_A   ,KC_S   ,KC_D   ,KC_F   ,KC_G   ,                KC_ACL1,KC_MS_L,KC_MS_D,KC_MS_R,KC_BTN4,KC_BTN7,
-        KC_LSFT,KC_Z   ,KC_X   ,KC_C   ,KC_V   ,KC_B   ,                KC_ACL0,KC_WH_L,KC_WH_U,KC_WH_R,KC_BTN5,KC_BTN8,
-                                KC_LALT,KC_LCTL,KC_SPC ,_______,XXXXXXX,KC_BTN1,KC_BTN3,KC_WH_D,
-                            _______,                    _______,XXXXXXX,                    KC_BTN2
+    [_NP] = LAYOUT(
+        _______,_______,_______,_______,_______,_______,                _______,_______,_______,_______,_______,_______,
+        KC_TAB ,KC_PSLS,KC_7   ,KC_8   ,KC_9   ,KC_PMNS,                KC_PSLS,KC_7   ,KC_8   ,KC_9   ,KC_PMNS,_______,
+        KC_CALC,KC_PAST,KC_4   ,KC_5   ,KC_6   ,KC_PPLS,                KC_PAST,KC_4   ,KC_5   ,KC_6   ,KC_PPLS,KC_CALC,
+        KC_LSFT,KC_PERC,KC_1   ,KC_2   ,KC_3   ,KC_EQL ,                KC_PERC,KC_1   ,KC_2   ,KC_3   ,KC_EQL ,_______,
+                                KC_0   ,KC_DOT ,_______,_______,_______,_______,KC_0   ,KC_DOT ,
+                            KC_ESC ,                    _______,_______,                    _______
     ),
     [_FN] = LAYOUT(
         KC_F1  ,KC_F2  ,KC_F3  ,KC_F4  ,KC_F5  ,KC_F6  ,                KC_F7  ,KC_F8  ,KC_F9  ,KC_F10 ,KC_F11 ,KC_F12 ,
-        _______,KC_PSLS,KC_7   ,KC_8   ,KC_9   ,KC_PMNS,                KC_BRIU,KC_HOME,KC_UP  ,KC_END ,KC_PGUP,KC_INS ,
+        KC_TAB ,KC_PSLS,KC_7   ,KC_8   ,KC_9   ,KC_PMNS,                KC_BRIU,KC_HOME,KC_UP  ,KC_END ,KC_PGUP,KC_INS ,
         KC_CALC,KC_PAST,KC_4   ,KC_5   ,KC_6   ,KC_PPLS,                KC_BRID,KC_LEFT,KC_DOWN,KC_RGHT,KC_PGDN,KC_DEL ,
-        _______,KC_PERC,KC_1   ,KC_2   ,KC_3   ,KC_EQL ,                KC_MPRV,KC_MPLY,KC_MNXT,KC_MUTE,KC_VOLD,KC_VOLU,
+        KC_LSFT,KC_PERC,KC_1   ,KC_2   ,KC_3   ,KC_EQL ,                KC_MPRV,KC_MPLY,KC_MNXT,KC_MUTE,KC_VOLD,KC_VOLU,
                                 KC_0   ,KC_DOT ,_______,_______,_______,_______,KC_RCTL,KC_RALT,
-                            _______,                    _______,_______,                    KC_CAPS
+                            KC_ESC ,                    _______,_______,                    KC_CAPS
     ),
     [MY_COMMAND] = LAYOUT(
         XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,                XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,
         XXXXXXX,TG(_QW),CG_NORM,TG(_FN),KC_PSCR,XXXXXXX,                XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,
-        XXXXXXX,XXXXXXX, RESET ,KC_SLCK,CG_TOGG,TG(GAME),               CG_SWAP,KC_NLCK,XXXXXXX,EEP_RST,XXXXXXX,XXXXXXX,
+        XXXXXXX,XXXXXXX, RESET ,KC_SLCK,CG_TOGG,TG(_GM),                CG_SWAP,KC_NLCK,XXXXXXX,EEP_RST,CG_TOGG,XXXXXXX,
         XXXXXXX,XXXXXXX,XXXXXXX,KC_CAPS,XXXXXXX,XXXXXXX,                XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,
                                 XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,
                             XXXXXXX,                    XXXXXXX,XXXXXXX,                    XXXXXXX
@@ -202,7 +204,7 @@ void td_copy_paste_finished(qk_tap_dance_state_t *state, void *user_data) {
     switch (td_state) {
         case SINGLE_TAP:  register_code16(C(KC_V));    break;
         case SINGLE_HOLD: tap_code16(C(KC_C));         break;
-        case DOUBLE_TAP:  layer_invert(NUM);           break;
+        case DOUBLE_TAP:  layer_invert(_NP);           break;
         default:          register_code16(LSG(KC_S)); return;
     }
 }
