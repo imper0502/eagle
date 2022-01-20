0fc5bb8cbc032a9b0474bab8a3b60ff0df477467
Jay_NTUT_LAB1321 <t109598063@ntut.org.tw>
2022-01-20 11:08:11 +0800

    💄 Update keymap of standard

--- keymaps/standard/keymap.c
+++ keymaps/standard/keymap.c
@@ -58,12 +58,13 @@ const key_override_t **key_overrides = (const key_override_t *[]) {
 #define TD_LANG TD(IME_CAPSLOCK)
 #define CPY_PST TD(COPY_PASTE_FNLOCK_SCREENSHOT)
 #define GUI_ESC LGUI_T(KC_ESC)
+#define OS_LSFT OSM(MOD_LSFT)
 #define OS_RSFT OSM(MOD_RSFT)
 #define GM_ESC  LT(_GM, KC_ESC)
 #define GM_TAB  LT(_GM, KC_TAB)
 #define FN_SPC  LT(_FN, KC_SPC)
 #define FN_RCTL LM(_FN, MOD_RCTL)
-#define FN_LALT LM(_FN, MOD_LALT)
+#define FN_RALT LM(_FN, MOD_RALT)
 
 /* Keymaps */
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
@@ -71,8 +72,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         ALT_TAB,KC_GRV ,KC_AT  ,KC_HASH,KC_AMPR,KC_SLSH,                KC_BSLS,KC_LPRN,KC_LBRC,KC_LCBR,KC_LABK,KC_BTN4,
         GM_TAB ,KC_Q   ,KC_W   ,KC_F   ,KC_P   ,KC_B   ,                KC_J   ,KC_L   ,KC_U   ,KC_Y   ,KC_MINS,KC_EQL ,
         GUI_ESC,KC_A   ,KC_R   ,KC_S   ,KC_T   ,KC_G   ,                KC_M   ,KC_N   ,KC_E   ,KC_I   ,KC_O   ,KC_QUES,
-        KC_LSFT,KC_Z   ,KC_X   ,KC_C   ,KC_D   ,KC_V   ,                KC_K   ,KC_H   ,KC_COMM,KC_DOT ,KC_QUOT,KC_RALT,
-                                KC_LALT,KC_LCTL,OS_RSFT,KC_BSPC,KC_ENT ,FN_SPC ,FN_RCTL,FN_LALT,
+        OS_RSFT,KC_Z   ,KC_X   ,KC_C   ,KC_D   ,KC_V   ,                KC_K   ,KC_H   ,KC_COMM,KC_DOT ,KC_QUOT,OS_LSFT,
+                                KC_LALT,KC_LCTL,KC_LSFT,KC_BSPC,KC_ENT ,FN_SPC ,FN_RCTL,FN_RALT,
                             GM_ESC,                     CPY_PST,KC_INS ,                    TD_LANG
     ),
     [_QW] = LAYOUT(
@@ -84,27 +85,27 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                             _______,                    _______,_______,                    _______
     ),
     [_GM] = LAYOUT(
-        KC_ESC ,KC_1   ,KC_2   ,KC_3   ,KC_4   ,KC_5   ,                KC_6   ,KC_7   ,KC_8   ,KC_9   ,KC_0   ,TG(_GM),
+        KC_ESC ,KC_1   ,KC_2   ,KC_3   ,KC_4   ,KC_5   ,                KC_6   ,KC_7   ,KC_8   ,KC_9   ,KC_0   ,XXXXXXX,
         KC_TAB ,KC_Q   ,KC_W   ,KC_E   ,KC_R   ,KC_T   ,                XXXXXXX,KC_WH_L,KC_MS_U,KC_WH_R,KC_WH_U,XXXXXXX,
-        KC_CAPS,KC_A   ,KC_S   ,KC_D   ,KC_F   ,KC_G   ,                XXXXXXX,KC_MS_L,KC_MS_D,KC_MS_R,KC_WH_D,XXXXXXX,
-        KC_LSFT,KC_Z   ,KC_X   ,KC_C   ,KC_V   ,KC_B   ,                XXXXXXX,KC_BTN6,KC_BTN7,KC_BTN8,XXXXXXX,XXXXXXX,
+        KC_CAPS,KC_A   ,KC_S   ,KC_D   ,KC_F   ,KC_G   ,                XXXXXXX,KC_MS_L,KC_MS_D,KC_MS_R,KC_WH_D,KC_ENT ,
+        KC_LSFT,KC_Z   ,KC_X   ,KC_C   ,KC_V   ,KC_B   ,                XXXXXXX,KC_BTN6,KC_BTN7,KC_BTN8,TG(_GM),KC_RSFT,
                                 _______,_______,_______,_______,KC_BTN4,KC_BTN1,KC_BTN3,KC_BTN2,
                             _______,                    _______,KC_BTN5,                    XXXXXXX
     ),
     [_NP] = LAYOUT(
         _______,_______,_______,_______,_______,_______,                _______,_______,_______,_______,_______,_______,
-        KC_TAB ,KC_PSLS,KC_7   ,KC_8   ,KC_9   ,KC_PMNS,                KC_PSLS,KC_7   ,KC_8   ,KC_9   ,KC_PMNS,_______,
-        KC_CALC,KC_PAST,KC_4   ,KC_5   ,KC_6   ,KC_PPLS,                KC_PAST,KC_4   ,KC_5   ,KC_6   ,KC_PPLS,KC_CALC,
-        KC_LSFT,KC_PERC,KC_1   ,KC_2   ,KC_3   ,KC_EQL ,                KC_PERC,KC_1   ,KC_2   ,KC_3   ,KC_EQL ,_______,
-                                KC_0   ,KC_DOT ,_______,_______,_______,_______,KC_0   ,KC_DOT ,
+        KC_TAB ,TG(_NP),KC_1   ,KC_2   ,KC_3   ,KC_BSLS,                KC_PSLS,KC_7   ,KC_8   ,KC_9   ,KC_PMNS,_______,
+        KC_CALC,KC_LABK,KC_4   ,KC_5   ,KC_6   ,KC_LPRN,                KC_PAST,KC_4   ,KC_5   ,KC_6   ,KC_PPLS,KC_CALC,
+        KC_LSFT,KC_LBRC,KC_7   ,KC_8   ,KC_9   ,KC_LCBR,                KC_PERC,KC_1   ,KC_2   ,KC_3   ,KC_EQL ,_______,
+                                KC_0   ,KC_PMNS,KC_PPLS,_______,_______,_______,KC_0   ,KC_DOT ,
                             KC_ESC ,                    _______,_______,                    _______
     ),
     [_FN] = LAYOUT(
         KC_F1  ,KC_F2  ,KC_F3  ,KC_F4  ,KC_F5  ,KC_F6  ,                KC_F7  ,KC_F8  ,KC_F9  ,KC_F10 ,KC_F11 ,KC_F12 ,
-        KC_TAB ,KC_PSLS,KC_7   ,KC_8   ,KC_9   ,KC_PMNS,                KC_BRIU,KC_HOME,KC_UP  ,KC_END ,KC_PGUP,KC_INS ,
-        KC_CALC,KC_PAST,KC_4   ,KC_5   ,KC_6   ,KC_PPLS,                KC_BRID,KC_LEFT,KC_DOWN,KC_RGHT,KC_PGDN,KC_DEL ,
-        KC_LSFT,KC_PERC,KC_1   ,KC_2   ,KC_3   ,KC_EQL ,                KC_MPRV,KC_MPLY,KC_MNXT,KC_MUTE,KC_VOLD,KC_VOLU,
-                                KC_0   ,KC_DOT ,_______,_______,_______,_______,KC_RCTL,KC_RALT,
+        KC_TAB ,TG(_NP),KC_1   ,KC_2   ,KC_3   ,KC_BSLS,                KC_BRIU,KC_HOME,KC_UP  ,KC_END ,KC_PGUP,KC_INS ,
+        KC_CALC,KC_LPRN,KC_4   ,KC_5   ,KC_6   ,KC_LBRC,                KC_BRID,KC_LEFT,KC_DOWN,KC_RGHT,KC_PGDN,KC_DEL ,
+        KC_LSFT,KC_LCBR,KC_7   ,KC_8   ,KC_9   ,KC_LABK,                KC_MUTE,KC_VOLD,KC_VOLU,KC_MPRV,KC_MPLY,KC_MNXT,
+                                KC_0   ,KC_PMNS,KC_PPLS,_______,_______,_______,KC_RCTL,KC_RALT,
                             KC_ESC ,                    _______,_______,                    KC_CAPS
     ),
     [MY_COMMAND] = LAYOUT(
@@ -142,7 +143,7 @@ bool get_tapping_force_hold(uint16_t keycode, keyrecord_t *record) {
     switch (keycode) {
     case FN_SPC :
     case FN_RCTL:
-    case FN_LALT:
+    case FN_RALT:
         return true;
     default:
         return false;
@@ -153,6 +154,7 @@ void post_process_record_user(uint16_t keycode, keyrecord_t *record) {
     switch (keycode) {
     case KC_LSFT:
     case KC_RSFT:
+    case OS_LSFT:
     case OS_RSFT:
         get_mods() == MOD_MASK_SHIFT ? layer_on(MY_COMMAND) : layer_off(MY_COMMAND);
         break;
