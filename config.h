147dd6d9a59d3bf1f0554ae7d76e542172be6132
Jay Wu <yoj845@gmail.com>
2022-02-12 16:09:11 +0800

    🔧 Changed pin defination for new MCU.

--- config.h
+++ config.h
@@ -40,8 +40,9 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
  *                  ROW2COL = ROW = Anode (+), COL = Cathode (-, marked on diode)
  *
  */
-#define MATRIX_ROW_PINS { F7, B1, B3, B2, B6, C6, D7, E6, B4, B5 }
-#define MATRIX_COL_PINS { F6, F5, F4, D4, D0, D1 }
+#define MATRIX_ROW_PINS { C6, D7, E6, B4, B5, F7, B1, B3, B2, B6 }
+#define MATRIX_COL_PINS { D1, D0, D4, F6, F5, F4 }
+
 #define UNUSED_PINS
 
 /* COL2ROW, ROW2COL */
