57fca612da19ec488b2ec39199f38f6819f6ec99
Jay_NTUT_LAB1321 <t109598063@ntut.org.tw>
2022-03-13 00:01:15 +0800

    ✨ Introduced F1~F12 Tap dance

--- keymaps/standard/keymap.c
+++ keymaps/standard/keymap.c
@@ -26,7 +26,10 @@ enum tap_dance_names {
     IME_CAPSLOCK,
     ALT_TABLE,
     COPY_PASTE_FNLOCK_SCREENSHOT,
-    INSERT_SCREENSHOT_NUMPAD
+    INSERT_SCREENSHOT_NUMPAD,
+    F_1,    F_2,    F_3,    F_4,
+    F_5,    F_6,    F_7,    F_8,
+    F_9,    F_10,   F_11,   F_12,
 };
 
 /* Key Override */
@@ -106,7 +109,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                           XXXXXXX,                            _______, KC_BTN4,                            XXXXXXX
     ),
     [_NP] = LAYOUT(
-        _______, _______, _______, _______, _______, _______,                   _______, _______, _______, _______, _______, KC_CALC,
+        TD(F_1), TD(F_2), TD(F_3), TD(F_4), TD(F_5), TD(F_6),                   TD(F_7), TD(F_8), TD(F_9),TD(F_10),TD(F_11),TD(F_12),
         KC_TAB , KC_PPLS, KC_7   , KC_8   , KC_9   , KC_PAST,                   KC_PAST, KC_7   , KC_8   , KC_9   , KC_PPLS, KC_TAB ,
         KC_ESC , KC_PMNS, KC_4   , KC_5   , KC_6   , KC_PSLS,                   KC_PSLS, KC_4   , KC_5   , KC_6   , KC_PMNS, KC_ESC ,
         KC_COMM, KC_EQL , KC_1   , KC_2   , KC_3   , KC_PERC,                   KC_PERC, KC_1   , KC_2   , KC_3   , KC_EQL , KC_COMM,
@@ -114,6 +117,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                           XXXXXXX,                            _______, TG(_NP),                            XXXXXXX
     ),
     [_FN] = LAYOUT(
+        TD(F_1), TD(F_2), TD(F_3), TD(F_4), TD(F_5), TD(F_6),                   TD(F_7), TD(F_8), TD(F_9),TD(F_10),TD(F_11),TD(F_12),
         KC_TAB , KC_PPLS, KC_7   , KC_8   , KC_9   , KC_PAST,                   KC_BRIU, KC_HOME, KC_UP  , KC_END , KC_PGUP, ALT_TAB,
         KC_ESC , KC_PMNS, KC_4   , KC_5   , KC_6   , KC_PSLS,                   KC_BRID, KC_LEFT, KC_DOWN, KC_RGHT, KC_PGDN, ALT_ESC,
         KC_COMM, KC_EQL , KC_1   , KC_2   , KC_3   , KC_PERC,                   KC_MUTE, KC_VOLD, KC_VOLU, KC_MPRV, KC_MPLY, KC_MNXT,
@@ -208,6 +212,18 @@ qk_tap_dance_action_t tap_dance_actions[] = {
     [ALT_TABLE] = ACTION_TAP_DANCE_FN_ADVANCED(td_alt_tab_each_tap, td_alt_tab_finished, NULL),
     [COPY_PASTE_FNLOCK_SCREENSHOT] = ACTION_TAP_DANCE_FN_ADVANCED(NULL, td_copy_paste_finished, td_copy_paste_reset),
     [INSERT_SCREENSHOT_NUMPAD] = ACTION_TAP_DANCE_FN_ADVANCED(NULL, td_insert_screenshot_finished, td_insert_screenshot_reset),
+    [F_1] = ACTION_TAP_DANCE_DOUBLE(KC_F1, KC_CALC),
+    [F_2] = ACTION_TAP_DANCE_DOUBLE(KC_F2, KC_CALC),
+    [F_3] = ACTION_TAP_DANCE_DOUBLE(KC_F3, KC_CALC),
+    [F_4] = ACTION_TAP_DANCE_DOUBLE(KC_F4, KC_CALC),
+    [F_5] = ACTION_TAP_DANCE_DOUBLE(KC_F5, KC_CALC),
+    [F_6] = ACTION_TAP_DANCE_DOUBLE(KC_F6, KC_CALC),
+    [F_7] = ACTION_TAP_DANCE_DOUBLE(KC_F7, KC_CALC),
+    [F_8] = ACTION_TAP_DANCE_DOUBLE(KC_F8, KC_CALC),
+    [F_9] = ACTION_TAP_DANCE_DOUBLE(KC_F9, KC_CALC),
+    [F_10] = ACTION_TAP_DANCE_DOUBLE(KC_F10, KC_CALC),
+    [F_11] = ACTION_TAP_DANCE_DOUBLE(KC_F11, KC_CALC),
+    [F_12] = ACTION_TAP_DANCE_DOUBLE(KC_F12, KC_CALC),
 };
 
 void td_alt_tab_each_tap(qk_tap_dance_state_t *state, void *user_data) {
