c4ca568ee78648b84d953254922a7b2d076d3353
Joey Wu <yoj845@gmail.com>
2022-11-04 00:19:15 +0800

    ✨ Changed Combo keys

--- keymaps/standard/combo.h
+++ keymaps/standard/combo.h
@@ -1,18 +1,16 @@
 #pragma once
 
 enum combos {
-  ST_MK_LAYER,
-  NE_MC_PLAY,
-  RST_SFT,
-  NEI_LAYER,
+  CD_MK_LAYER,
+  HCOMMA_MC_PLAY,
   COMBO_LENGTH
 };
 uint16_t COMBO_LEN = COMBO_LENGTH;
 
-const uint16_t PROGMEM st_combo[] = {KC_S, KC_T, COMBO_END};
-const uint16_t PROGMEM ne_combo[] = {KC_N, KC_E, COMBO_END};
+const uint16_t PROGMEM left_combo[] = {KC_C, KC_D, COMBO_END};
+const uint16_t PROGMEM right_combo[] = {KC_H, KC_COMM, COMBO_END};
 
 combo_t key_combos[] = {
-    [ST_MK_LAYER] = COMBO(st_combo, MO(_MK)),
-    [NE_MC_PLAY] = COMBO(ne_combo, DM_PLY1),
+    [CD_MK_LAYER] = COMBO(left_combo, MO(_MK)),
+    [HCOMMA_MC_PLAY] = COMBO(right_combo, DM_PLY1),
 };
